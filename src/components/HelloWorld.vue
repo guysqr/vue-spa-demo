67d16f7edf72aa2b9b31ddb9702ec217681abd20
Guy Morton <guy@Guys-Versent-MacBook-Pro.local>
2021-06-29 08:25:46 +1000

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      Woot. For a guide and recipes on how to configure / customize this
+      Here's an update. For a guide and recipes on how to configure / customize this
       project,<br />
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
